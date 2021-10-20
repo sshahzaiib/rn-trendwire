51f44d544a1f72cf0cc4078a97ad3317e6d7cfc1
Shahzaib <zaibansari20@gmail.com>
2021-10-20 19:41:01 +0500

    Trendwire package for vendors - com.trendwire.vendor

--- ios/TWVendorTests/TWVendorTests.m
+++ ios/TWVendorTests/TWVendorTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface trendwireTests : XCTestCase
+@interface TWVendorTests : XCTestCase
 
 @end
 
-@implementation trendwireTests
+@implementation TWVendorTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
